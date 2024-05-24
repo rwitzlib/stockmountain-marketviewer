fbeaa4dfcb425c24d043cfc97d23a75d8a2b983d
RobWitzlib <rob.witzlib@gmail.com>
2024-05-23 21:55:19 -0500

    create initial request and models

---
+++ src/MarketViewer.Contracts/Models/Scan/StudyOperand.cs
@@ -0,0 +1,11 @@
+﻿using MarketViewer.Contracts.Enums;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MarketViewer.Contracts.Models.Scan;
+
+[ExcludeFromCodeCoverage]
+public class StudyOperand
+{
+    public StudyType Type { get; set; }
+    public string[] Parameters { get; set; }
+}
---
+++ src/MarketViewer.Contracts/Requests/ScannerRequest.cs
@@ -0,0 +1,11 @@
+﻿using MarketViewer.Contracts.Models.Scan;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MarketViewer.Contracts.Requests;
+
+[ExcludeFromCodeCoverage]
+public class ScannerRequest
+{
+    public ScanArgument Arguments { get; set; }
+    public string Timestamp { get; set; }
+}
