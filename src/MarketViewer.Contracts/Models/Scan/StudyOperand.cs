67887c91169e96975033b68948b4dd1849865945
RobWitzlib <rob.witzlib@gmail.com>
2024-06-15 12:09:57 -0500

    add request parsing, add more logic to handlers fixing and or logic

--- src/MarketViewer.Contracts/Models/Scan/StudyOperand.cs
+++ src/MarketViewer.Contracts/Models/Scan/StudyOperand.cs
@@ -1,15 +1,25 @@
 ﻿using MarketViewer.Contracts.Enums;
 using MarketViewer.Contracts.Responses;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 namespace MarketViewer.Contracts.Models.Scan;
 
 [ExcludeFromCodeCoverage]
 public class StudyOperand : IScanOperand
 {
+    [JsonRequired]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public StudyType Type { get; set; }
+
+    [JsonRequired]
     public string[] Parameters { get; set; }
+
+    [JsonRequired]
     public int Multiplier { get; set; }
+
+    [JsonRequired]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public Timespan Timespan { get; set; }
 
     public float[] Compute(StocksResponse stocksResponse, Timeframe timeframe)
--- src/MarketViewer.Contracts/Models/Scan/ValueOperand.cs
+++ src/MarketViewer.Contracts/Models/Scan/ValueOperand.cs
@@ -1,17 +1,19 @@
 ﻿using MarketViewer.Contracts.Enums;
 using MarketViewer.Contracts.Responses;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 namespace MarketViewer.Contracts.Models.Scan;
 
 [ExcludeFromCodeCoverage]
 public class ValueOperand : IScanOperand
 {
+    [JsonRequired]
     public float Value { get; set; }
 
     public float[] Compute(StocksResponse stocksResponse, Timeframe timeframe)
     {
-        throw new System.NotImplementedException();
+        return [ Value ];
     }
 
     public bool HasTimespan(out Timespan? timespan)
