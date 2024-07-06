3544c9215caea5329dd8f0d5c5bdb7ee5187f1ce
RobWitzlib <rob.witzlib@gmail.com>
2024-07-05 20:02:44 -0500

    add converter

---
+++ src/MarketViewer.Api.UnitTests/Converters/ScanArgumentConverterUnitTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using MarketViewer.Contracts.Converters;
+using MarketViewer.Contracts.Enums;
+using MarketViewer.Contracts.Models.Scan;
+using MarketViewer.Contracts.Models.ScanV2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MarketViewer.Api.UnitTests.Converters;
+
+public class ScanArgumentConverterUnitTests
+{
+    private readonly JsonSerializerOptions _options = new JsonSerializerOptions
+    {
+        
+    };
+
+    public ScanArgumentConverterUnitTests()
+    {
+        _options.Converters.Add(new ScanArgumentConverter()); 
+    }
+
+    [Fact]
+    public void Serialize()
+    {
+        // Arrange
+        var json = "{\r\n    \"Operator\": \"AND\",\r\n    \"Filters\": [\r\n      {\r\n        \"CollectionModifier\": \"ALL\",\r\n        \"FirstOperand\": {\r\n          \"ValueType\": \"Value\",\r\n          \"Type\": \"Close\",\r\n          \"Multiplier\": 1,\r\n          \"Timespan\": \"minute\"\r\n        },\r\n        \"Operator\": \"gt\",\r\n        \"SecondOperand\": {\r\n          \"Value\": 5\r\n        },\r\n        \"Timeframe\": {\r\n          \"Multiplier\": 5,\r\n          \"Timespan\": \"minute\"\r\n        }\r\n      },\r\n      {\r\n        \"CollectionModifier\": \"ALL\",\r\n        \"FirstOperand\": {\r\n          \"ValueType\": \"Value\",\r\n          \"Type\": \"macd\",\r\n          \"Parameters\": \"12,26,9,EMA\",\r\n          \"Multiplier\": 1,\r\n          \"Timespan\": \"minute\"\r\n        },\r\n        \"Operator\": \"lt\",\r\n        \"SecondOperand\": {\r\n          \"Value\": 0\r\n        },\r\n        \"Timeframe\": {\r\n          \"Multiplier\": 5,\r\n          \"Timespan\": \"minute\"\r\n        }\r\n      }\r\n      \r\n    ],\r\n    \"Argument\": {\r\n      \"Operator\": \"AND\",\r\n      \"Filters\": [\r\n        {\r\n          \"CollectionModifier\": \"ALL\",\r\n          \"FirstOperand\": {\r\n            \"ValueType\": \"Value\",\r\n            \"Type\": \"Close\",\r\n            \"Multiplier\": 1,\r\n            \"Timespan\": \"minute\"\r\n          },\r\n          \"Operator\": \"lt\",\r\n          \"SecondOperand\": {\r\n            \"Value\": 10      \r\n          },\r\n          \"Timeframe\": {\r\n            \"Multiplier\": 5,\r\n            \"Timespan\": \"minute\"\r\n          }\r\n        }\r\n      ]\r\n    }\r\n  }";
+        var deserialized = JsonSerializer.Deserialize<ScanArgument>(json, _options);
+        var serialized = JsonSerializer.Serialize(deserialized, _options);
+
+        // Act
+        var argument = JsonSerializer.Deserialize<ScanArgument>(serialized, _options);
+
+        // Assert
+        argument.Operator.Should().Be("AND");
+
+        argument.Filters.Should().NotBeNull();
+        argument.Filters.Length.Should().Be(2);
+
+        var firstOperand = argument.Filters[0].FirstOperand.Should().BeOfType<PriceActionOperand>().Subject;
+        firstOperand.ValueType.Should().Be(Modifier.Value);
+        firstOperand.Type.Should().Be(PriceActionType.Close);
+        firstOperand.Multiplier.Should().Be(1);
+        firstOperand.Timespan.Should().Be(Timespan.minute);
+
+        argument.Argument.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Deserialization()
+    {
+        // Arrange
+        var json = "{\r\n    \"Operator\": \"AND\",\r\n    \"Filters\": [\r\n      {\r\n        \"CollectionModifier\": \"ALL\",\r\n        \"FirstOperand\": {\r\n          \"ValueType\": \"Value\",\r\n          \"Type\": \"Close\",\r\n          \"Multiplier\": 1,\r\n          \"Timespan\": \"minute\"\r\n        },\r\n        \"Operator\": \"gt\",\r\n        \"SecondOperand\": {\r\n          \"Value\": 5\r\n        },\r\n        \"Timeframe\": {\r\n          \"Multiplier\": 5,\r\n          \"Timespan\": \"minute\"\r\n        }\r\n      },\r\n      {\r\n        \"CollectionModifier\": \"ALL\",\r\n        \"FirstOperand\": {\r\n          \"ValueType\": \"Value\",\r\n          \"Type\": \"macd\",\r\n          \"Parameters\": \"12,26,9,EMA\",\r\n          \"Multiplier\": 1,\r\n          \"Timespan\": \"minute\"\r\n        },\r\n        \"Operator\": \"lt\",\r\n        \"SecondOperand\": {\r\n          \"Value\": 0\r\n        },\r\n        \"Timeframe\": {\r\n          \"Multiplier\": 5,\r\n          \"Timespan\": \"minute\"\r\n        }\r\n      }\r\n      \r\n    ],\r\n    \"Argument\": {\r\n      \"Operator\": \"AND\",\r\n      \"Filters\": [\r\n        {\r\n          \"CollectionModifier\": \"ALL\",\r\n          \"FirstOperand\": {\r\n            \"ValueType\": \"Value\",\r\n            \"Type\": \"Close\",\r\n            \"Multiplier\": 1,\r\n            \"Timespan\": \"minute\"\r\n          },\r\n          \"Operator\": \"lt\",\r\n          \"SecondOperand\": {\r\n            \"Value\": 10      \r\n          },\r\n          \"Timeframe\": {\r\n            \"Multiplier\": 5,\r\n            \"Timespan\": \"minute\"\r\n          }\r\n        }\r\n      ]\r\n    }\r\n  }";
+        
+        // Act
+        var argument = JsonSerializer.Deserialize<ScanArgument>(json, _options);
+
+        // Assert
+        argument.Operator.Should().Be("AND");
+
+        argument.Filters.Should().NotBeNull();
+        argument.Filters.Length.Should().Be(2);
+
+        var firstOperand = argument.Filters[0].FirstOperand.Should().BeOfType<PriceActionOperand>().Subject;
+        firstOperand.ValueType.Should().Be(Modifier.Value);
+        firstOperand.Type.Should().Be(PriceActionType.Close);
+        firstOperand.Multiplier.Should().Be(1);
+        firstOperand.Timespan.Should().Be(Timespan.minute);
+
+        argument.Argument.Should().NotBeNull();
+    }
+}
---
+++ src/MarketViewer.Contracts/Converters/ScanArgumentConverter.cs
@@ -0,0 +1,136 @@
+﻿using MarketViewer.Contracts.Models.Scan;
+using MarketViewer.Contracts.Models.ScanV2;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace MarketViewer.Contracts.Converters;
+
+public class ScanArgumentConverter : System.Text.Json.Serialization.JsonConverter<ScanArgument>
+{
+    public ScanArgumentConverter() { }
+
+    public override ScanArgument Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var document = JsonDocument.ParseValue(ref reader);
+        var jsonElement = document.RootElement;
+
+        var argument = ParseArgument(jsonElement);
+
+        return argument;
+    }
+
+    public override void Write(Utf8JsonWriter writer, ScanArgument value, JsonSerializerOptions options)
+    {
+        if (value is null)
+        {
+            writer.WriteNullValue();
+        }
+        else
+        {
+            var json = JsonConvert.SerializeObject(value);
+            writer.WriteRawValue(json);
+        }
+    }
+
+    private static ScanArgument ParseArgument(JsonElement jsonElement)
+    {
+        if (jsonElement.ValueKind == JsonValueKind.Null)
+        {
+            return null;
+        }
+
+        var scanArgument = new ScanArgument();
+
+        if (jsonElement.TryGetProperty("Argument", out var argument))
+        {
+            scanArgument.Argument = ParseArgument(argument);
+        }
+
+        if (jsonElement.TryGetProperty("Operator", out var argOperator))
+        {
+            scanArgument.Operator = argOperator.GetString();
+        }
+
+        if (jsonElement.TryGetProperty("Filters", out var filters))
+        {
+            var enumerator = filters.EnumerateArray();
+
+            var count = filters.GetArrayLength();
+
+            scanArgument.Filters = new FilterV2[count];
+            for (int i = 0; i < count; i++)
+            {
+                enumerator.MoveNext();
+                scanArgument.Filters[i] = ParseFilter(enumerator.Current);
+            }
+        }
+
+        return scanArgument;
+    }
+
+    private static FilterV2 ParseFilter(JsonElement jsonElement)
+    {
+        var filter = new FilterV2();
+
+        if (jsonElement.TryGetProperty("CollectionModifier", out var modifierElement))
+        {
+            filter.CollectionModifier = modifierElement.GetString();
+        }
+
+        if (jsonElement.TryGetProperty("FirstOperand", out var firstOperand))
+        {
+            filter.FirstOperand = ParseOperand(firstOperand);
+        }
+
+        if (jsonElement.TryGetProperty("Operator", out var operatorElement) && Enum.TryParse<FilterOperator>(operatorElement.ToString(), out var filterOperator))
+        {
+            filter.Operator = filterOperator;
+        }
+
+        if (jsonElement.TryGetProperty("SecondOperand", out var secondOperand))
+        {
+            filter.SecondOperand = ParseOperand(secondOperand);
+        }
+
+        if (jsonElement.TryGetProperty("Timeframe", out var timeframe))
+        {
+            filter.Timeframe = JsonSerializer.Deserialize<Timeframe>(timeframe.GetRawText());
+        }
+
+        return filter;
+    }
+
+    private static IScanOperand ParseOperand(JsonElement jsonElement)
+    {
+        var json = jsonElement.GetRawText();
+
+        if (typeof(StudyOperand).GetProperties()
+            .Select(q => q.Name)
+            .All(prop => jsonElement.TryGetProperty(prop, out var result) == true))
+        {
+            var operand = JsonSerializer.Deserialize<StudyOperand>(json);
+            return operand;
+        }
+
+        if (typeof(PriceActionOperand).GetProperties()
+            .Select(q => q.Name)
+            .All(prop => jsonElement.TryGetProperty(prop, out var result) == true))
+        {
+            var operand = JsonSerializer.Deserialize<PriceActionOperand>(json);
+            return operand;
+        }
+
+        if (typeof(ValueOperand).GetProperties()
+            .Select(q => q.Name)
+            .All(prop => jsonElement.TryGetProperty(prop, out var result) == true))
+        {
+            var operand = JsonSerializer.Deserialize<ValueOperand>(json);
+            return operand;
+        }
+
+        throw new NotImplementedException();
+    }
+}
