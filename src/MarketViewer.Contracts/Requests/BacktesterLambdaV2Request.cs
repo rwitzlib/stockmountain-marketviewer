5896da8f5caf5db43584a1f876d44b872de58775
RobWitzlib <rob.witzlib@gmail.com>
2024-07-05 20:02:58 -0500

    add argument to contracts

---
+++ src/MarketViewer.Contracts/Requests/BacktesterLambdaV2Request.cs
@@ -0,0 +1,19 @@
+﻿using MarketViewer.Contracts.Models.Backtest;
+using MarketViewer.Contracts.Models.Scan;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MarketViewer.Contracts.Requests;
+
+[ExcludeFromCodeCoverage]
+public class BacktesterLambdaV2Request
+{
+    public DateTimeOffset Timestamp { get; set; }
+    public string ExitStrategy { get; set; }
+    public string ExitType { get; set; }
+    public float PositionSize { get; set; }
+    public int Candles { get; set; }
+    public ScanArgument Argument { get; set; }
+    public IEnumerable<Feature> Features { get; set; }
+}
--- src/MarketViewer.Contracts/Requests/ScanV2Request.cs
+++ src/MarketViewer.Contracts/Requests/ScanV2Request.cs
@@ -1,15 +1,18 @@
-﻿using MarketViewer.Contracts.Models;
+﻿using MarketViewer.Contracts.Converters;
+using MarketViewer.Contracts.Models;
 using MarketViewer.Contracts.Models.Scan;
 using MarketViewer.Contracts.Responses;
 using MediatR;
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 namespace MarketViewer.Contracts.Requests;
 
 [ExcludeFromCodeCoverage]
 public class ScanRequestV2 : IRequest<OperationResult<ScanResponse>>
 {
+    [JsonConverter(typeof(ScanArgumentConverter))]
     public ScanArgument Argument { get; set; }
     public DateTimeOffset Timestamp { get; set; }
 }
