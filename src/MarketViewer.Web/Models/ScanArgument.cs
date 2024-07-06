b14188086a29f5cd8bbccb5566376c62f851c47e
RobWitzlib <rob.witzlib@gmail.com>
2024-07-05 20:03:21 -0500

    add backtest v2 handler

---
+++ src/MarketViewer.Web/Models/ScanArgument.cs
@@ -0,0 +1,9 @@
+﻿namespace MarketViewer.Web.Models
+{
+    public class ScanArgument
+    {
+        public string Id { get; set; }
+        public string Operator { get; set; }
+        public ScanArgument Argument { get; set; }
+    }
+}
