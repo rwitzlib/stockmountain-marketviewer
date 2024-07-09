658c673eefc5b7beb92f03225bdfa2480d3359c3
RobWitzlib <rob.witzlib@gmail.com>
2024-07-09 08:36:41 -0500

    can add arguments, filters, drag them

--- src/MarketViewer.Web/Models/ScanArgument.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MarketViewer.Web.Models
-{
-    public class ScanArgument
-    {
-        public string Id { get; set; }
-        public string Operator { get; set; }
-        public ScanArgument Argument { get; set; }
-    }
-}
