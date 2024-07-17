219ec89db6c51aca13a1027b67c6384ee55e0041
RobWitzlib <rob.witzlib@gmail.com>
2024-07-17 08:08:49 -0500

    add initial filter parameters

--- src/MarketViewer.Web/Models/FilterItem.cs
+++ src/MarketViewer.Web/Models/FilterItem.cs
@@ -1,17 +1,21 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using MarketViewer.Contracts.Models.ScanV2;
+using System.Diagnostics.CodeAnalysis;
 
 namespace MarketViewer.Web.Models;
 
 [ExcludeFromCodeCoverage]
 public class FilterItem : IDisposable
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string CollectionModifier { get; set; }
+    public OperandType FirstOperandType { get; set; }
+    public IScanOperand FirstOperand { get; set; }
+    public OperandType SecondOperandType { get; set; }
+    public IScanOperand SecondOperand { get; set; }
+    public Timeframe Timeframe { get; set; } = new();
 
     public FilterItem()
     {
-        Id = Guid.NewGuid().ToString();
-        Name = $"Filter {Name}";
     }
 
     public void Dispose()
