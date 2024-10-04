6024de6f6983ee0f69b9a04a9c434b45e91f5b19
RobWitzlib <rob.witzlib@gmail.com>
2024-10-04 17:12:21 -0500

    add backtester v3 request

---
+++ src/MarketViewer.Contracts/Requests/Backtest/BacktesterLambdaV3Request.cs
@@ -0,0 +1,16 @@
+﻿using MarketViewer.Contracts.Models.Backtest;
+using MarketViewer.Contracts.Models.ScanV2;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MarketViewer.Contracts.Requests.Backtest;
+
+[ExcludeFromCodeCoverage]
+public class BacktesterLambdaV3Request
+{
+    public DateTimeOffset Timestamp { get; set; }
+    public bool DetailedResponse { get; set; } = false;
+    public BacktestPosition PositionInfo { get; set; }
+    public BacktestExit Exit { get; set; }
+    public IEnumerable<Feature> Features { get; set; }
+    public ScanArgument Argument { get; set; }
+}
