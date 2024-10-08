6652ed5ab5de813fa25a2b8f1a3253eda52ca0f6
RobWitzlib <rob.witzlib@gmail.com>
2024-10-08 07:11:58 -0500

    build response object, remove "other" field from backtest v2

--- src/MarketViewer.Contracts/Responses/Backtest/BacktestV3Response.cs
+++ src/MarketViewer.Contracts/Responses/Backtest/BacktestV3Response.cs
@@ -1,16 +1,51 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using MarketViewer.Contracts.Models.Backtest;
 
-namespace MarketViewer.Contracts.Responses.Backtest
+namespace MarketViewer.Contracts.Responses.Backtest;
+
+[ExcludeFromCodeCoverage]
+public class BacktestV3Response
 {
-    [ExcludeFromCodeCoverage]
-    public class BacktestV3Response
-    {
-        public string Id { get; set; }
-        public float HoldBalance { get; set; }
-        public float HighBalance { get; set; }
-        public BackTestEntryStatsV3 Hold { get; set; }
-        public BackTestEntryStatsV3 High { get; set; }
-        public IEnumerable<BacktestEntryV3> Results { get; set; }
-    }
+    public string Id { get; set; }
+    public float StartBalance { get; set; }
+    public float HoldBalance { get; set; }
+    public float HighBalance { get; set; }
+    public float PotentialHoldProfit { get; set; }
+    public float PotentialHighProfit { get; set; }
+    public BackTestEntryStatsV3 Hold { get; set; }
+    public BackTestEntryStatsV3 High { get; set; }
+    public IEnumerable<BacktestDayV3> Results { get; set; }
+    public IEnumerable<BacktestEntryV3> Other { get; set; }
+}
+
+[ExcludeFromCodeCoverage]
+public class BacktestDayV3
+{
+    public DateTimeOffset Date { get; set; }
+    public float CreditsUsed { get; set; }
+    public BacktestDayDetails Hold { get; set; }
+    public BacktestDayDetails High { get; set; }
+}
+
+[ExcludeFromCodeCoverage]
+public class BacktestDayDetails
+{
+    public float StartingBalance { get; set; }
+    public float EndingBalance { get; set; }
+    public float Profit { get; set; }
+    public int OpenPositions { get; set; }
+    public List<BacktestDayPosition> Bought { get; set; }
+    public List<BacktestDayPosition> Sold { get; set; }
+}
+
+[ExcludeFromCodeCoverage]
+public class BacktestDayPosition
+{
+    public string Ticker { get; set; }
+    public float Price { get; set; }
+    public int Shares { get; set; }
+    public float Position { get; set; }
+    public float Profit { get; set; }
+    public DateTimeOffset Timestamp { get; set; }
+    public bool StoppedOut { get; set; }
 }
