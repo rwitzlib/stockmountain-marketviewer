c52e652a87eabc7504a292618bdaa98d6ec84d53
RobWitzlib <rob.witzlib@gmail.com>
2024-10-31 09:24:44 -0500

    create dynamodb table

--- tf/dynamodb.tf
+++ tf/dynamodb.tf
@@ -36,3 +36,29 @@ resource "aws_dynamodb_table" "backtest" {
     projection_type = "ALL"
   }
 }
+
+resource "aws_dynamodb_table" "trade" {
+  name           = "${var.team}-${var.environment}-${var.service_name}-trade-store"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "Id"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+
+  attribute {
+    name = "CustomerId"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "CustomerIndex"
+    hash_key        = "CustomerId"
+    write_capacity  = 1
+    read_capacity   = 1
+    projection_type = "ALL"
+  }
+}
