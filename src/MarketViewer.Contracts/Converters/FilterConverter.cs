56f82dffa9e2fb572d9e5ef693b991ec2cf346f5
RobWitzlib <rob.witzlib@gmail.com>
2024-11-09 20:37:53 -0600

    add new converter, publish new prerealse

---
+++ src/MarketViewer.Contracts/Converters/FilterConverter.cs
@@ -0,0 +1,95 @@
+﻿using MarketViewer.Contracts.Enums;
+using MarketViewer.Contracts.Models.ScanV2;
+using MarketViewer.Contracts.Models.ScanV2.Operands;
+using Newtonsoft.Json;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace MarketViewer.Contracts.Converters;
+
+public class FilterConverter : System.Text.Json.Serialization.JsonConverter<FilterV2>
+{
+    public FilterConverter() { }
+
+    public override FilterV2 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var document = JsonDocument.ParseValue(ref reader);
+        var jsonElement = document.RootElement;
+
+        var argument = ParseFilter(jsonElement);
+
+        return argument;
+    }
+
+    public override void Write(Utf8JsonWriter writer, FilterV2 value, JsonSerializerOptions options)
+    {
+        if (value is null)
+        {
+            writer.WriteNullValue();
+        }
+        else
+        {
+            var json = JsonConvert.SerializeObject(value);
+            writer.WriteRawValue(json);
+        }
+    }
+
+    public static FilterV2 ParseFilter(JsonElement jsonElement)
+    {
+        var filter = new FilterV2();
+
+        if (jsonElement.TryGetProperty("CollectionModifier", out var modifierElement))
+        {
+            filter.CollectionModifier = modifierElement.GetString();
+        }
+
+        if (jsonElement.TryGetProperty("FirstOperand", out var firstOperand))
+        {
+            filter.FirstOperand = ParseOperand(firstOperand);
+        }
+
+        if (jsonElement.TryGetProperty("Operator", out var operatorElement) && Enum.TryParse<FilterOperator>(operatorElement.ToString(), out var filterOperator))
+        {
+            filter.Operator = filterOperator;
+        }
+
+        if (jsonElement.TryGetProperty("SecondOperand", out var secondOperand))
+        {
+            filter.SecondOperand = ParseOperand(secondOperand);
+        }
+
+        if (jsonElement.TryGetProperty("Timeframe", out var timeframe))
+        {
+            filter.Timeframe = JsonSerializer.Deserialize<Timeframe>(timeframe.GetRawText());
+        }
+
+        return filter;
+    }
+
+    private static IScanOperand ParseOperand(JsonElement jsonElement)
+    {
+        var json = jsonElement.GetRawText();
+
+        if (jsonElement.TryGetProperty("Study", out var studyElement))
+        {
+            return JsonSerializer.Deserialize<StudyOperand>(json);
+        }
+
+        if (jsonElement.TryGetProperty("PriceAction", out var priceAction))
+        {
+            return JsonSerializer.Deserialize<PriceActionOperand>(json);
+        }
+
+        if (jsonElement.TryGetProperty("Property", out var property))
+        {
+            return JsonSerializer.Deserialize<PropertyOperand>(json);
+        }
+
+        if (jsonElement.TryGetProperty("Value", out var value))
+        {
+            return JsonSerializer.Deserialize<FixedOperand>(json);
+        }
+
+        return null;
+    }
+}
--- src/MarketViewer.Contracts/Converters/ScanArgumentConverter.cs
+++ src/MarketViewer.Contracts/Converters/ScanArgumentConverter.cs
@@ -2,18 +2,12 @@
 using MarketViewer.Contracts.Models.ScanV2;
 using MarketViewer.Contracts.Models.ScanV2.Operands;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Reflection.Metadata.Ecma335;
 using System.Text.Json;
-using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace MarketViewer.Contracts.Converters;
 
 public class ScanArgumentConverter : System.Text.Json.Serialization.JsonConverter<ScanArgument>
 {
-    public ScanArgumentConverter() { }
-
     public override ScanArgument Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         var document = JsonDocument.ParseValue(ref reader);
@@ -66,69 +60,10 @@ public class ScanArgumentConverter : System.Text.Json.Serialization.JsonConverte
             for (int i = 0; i < count; i++)
             {
                 enumerator.MoveNext();
-                scanArgument.Filters.Add(ParseFilter(enumerator.Current));
+                scanArgument.Filters.Add(FilterConverter.ParseFilter(enumerator.Current));
             }
         }
 
         return scanArgument;
-    }
-
-    private static FilterV2 ParseFilter(JsonElement jsonElement)
-    {
-        var filter = new FilterV2();
-
-        if (jsonElement.TryGetProperty("CollectionModifier", out var modifierElement))
-        {
-            filter.CollectionModifier = modifierElement.GetString();
-        }
-
-        if (jsonElement.TryGetProperty("FirstOperand", out var firstOperand))
-        {
-            filter.FirstOperand = ParseOperand(firstOperand);
-        }
-
-        if (jsonElement.TryGetProperty("Operator", out var operatorElement) && Enum.TryParse<FilterOperator>(operatorElement.ToString(), out var filterOperator))
-        {
-            filter.Operator = filterOperator;
-        }
-
-        if (jsonElement.TryGetProperty("SecondOperand", out var secondOperand))
-        {
-            filter.SecondOperand = ParseOperand(secondOperand);
-        }
-
-        if (jsonElement.TryGetProperty("Timeframe", out var timeframe))
-        {
-            filter.Timeframe = JsonSerializer.Deserialize<Timeframe>(timeframe.GetRawText());
-        }
-
-        return filter;
-    }
-
-    private static IScanOperand ParseOperand(JsonElement jsonElement)
-    {
-        var json = jsonElement.GetRawText();
-
-        if (jsonElement.TryGetProperty("Study", out var studyElement))
-        {
-            return JsonSerializer.Deserialize<StudyOperand>(json);
-        }
-
-        if (jsonElement.TryGetProperty("PriceAction", out var priceAction))
-        {
-            return JsonSerializer.Deserialize<PriceActionOperand>(json);
-        }
-
-        if (jsonElement.TryGetProperty("Property", out var property))
-        {
-            return JsonSerializer.Deserialize<PropertyOperand>(json);
-        }
-
-        if (jsonElement.TryGetProperty("Value", out var value))
-        {
-            return JsonSerializer.Deserialize<FixedOperand>(json);
-        }
-
-        return null;
     }
 }
