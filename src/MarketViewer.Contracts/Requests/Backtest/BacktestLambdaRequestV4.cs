c199642ddffd4bd8010d2327e76f5e4ee10ebe54
rwitzlib <rob.witzlib@protonmail.com>
2024-11-28 22:26:28 -0600

    add ticker list to backtest request

---
+++ src/MarketViewer.Contracts/Requests/Backtest/BacktestLambdaRequestV4.cs
@@ -0,0 +1,17 @@
+﻿using MarketViewer.Contracts.Models.Backtest;
+using MarketViewer.Contracts.Models.ScanV2;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MarketViewer.Contracts.Requests.Backtest;
+
+[ExcludeFromCodeCoverage]
+public class BacktestLambdaRequestV4
+{
+    public DateTimeOffset Date { get; set; }
+    public IEnumerable<string> Tickers { get; set; }
+    public bool DetailedResponse { get; set; } = false;
+    public BacktestPositionInformation PositionInfo { get; set; }
+    public BacktestExitInformation Exit { get; set; }
+    public IEnumerable<Feature> Features { get; set; }
+    public ScanArgument Argument { get; set; }
+}
