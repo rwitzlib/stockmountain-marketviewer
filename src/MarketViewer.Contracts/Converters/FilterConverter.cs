d8225ca968e024cdfc6b2141c95fab57f5e45444
Rob Witzlib <61067692+rwitzlib@users.noreply.github.com>
2024-12-14 18:32:46 -0600

    remove newtonsoft from converter

--- src/MarketViewer.Contracts/Converters/FilterConverter.cs
+++ src/MarketViewer.Contracts/Converters/FilterConverter.cs
@@ -2,22 +2,20 @@
 using MarketViewer.Contracts.Models.Scan;
 using MarketViewer.Contracts.Models.ScanV2;
 using MarketViewer.Contracts.Models.ScanV2.Operands;
-using Newtonsoft.Json;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace MarketViewer.Contracts.Converters;
 
-public class FilterConverter : System.Text.Json.Serialization.JsonConverter<FilterV2>
+public class FilterConverter : JsonConverter<FilterV2>
 {
-    public FilterConverter() { }
-
     public override FilterV2 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         var document = JsonDocument.ParseValue(ref reader);
         var jsonElement = document.RootElement;
 
-        var argument = ParseFilter(jsonElement);
+        var argument = ParseFilter(jsonElement, options);
 
         return argument;
     }
@@ -30,12 +28,12 @@ public class FilterConverter : System.Text.Json.Serialization.JsonConverter<Filt
         }
         else
         {
-            var json = JsonConvert.SerializeObject(value);
+            var json = JsonSerializer.Serialize(value, options);
             writer.WriteRawValue(json);
         }
     }
 
-    public static FilterV2 ParseFilter(JsonElement jsonElement)
+    public static FilterV2 ParseFilter(JsonElement jsonElement, JsonSerializerOptions options)
     {
         var filter = new FilterV2();
 
@@ -46,7 +44,7 @@ public class FilterConverter : System.Text.Json.Serialization.JsonConverter<Filt
 
         if (jsonElement.TryGetProperty("FirstOperand", out var firstOperand))
         {
-            filter.FirstOperand = ParseOperand(firstOperand);
+            filter.FirstOperand = ParseOperand(firstOperand, options);
         }
 
         if (jsonElement.TryGetProperty("Operator", out var operatorElement) && Enum.TryParse<FilterOperator>(operatorElement.ToString(), out var filterOperator))
@@ -56,39 +54,39 @@ public class FilterConverter : System.Text.Json.Serialization.JsonConverter<Filt
 
         if (jsonElement.TryGetProperty("SecondOperand", out var secondOperand))
         {
-            filter.SecondOperand = ParseOperand(secondOperand);
+            filter.SecondOperand = ParseOperand(secondOperand, options);
         }
 
         if (jsonElement.TryGetProperty("Timeframe", out var timeframe))
         {
-            filter.Timeframe = JsonSerializer.Deserialize<Timeframe>(timeframe.GetRawText());
+            filter.Timeframe = JsonSerializer.Deserialize<Timeframe>(timeframe.GetRawText(), options);
         }
 
         return filter;
     }
 
-    private static IScanOperand ParseOperand(JsonElement jsonElement)
+    private static IScanOperand ParseOperand(JsonElement jsonElement, JsonSerializerOptions options)
     {
         var json = jsonElement.GetRawText();
 
         if (jsonElement.TryGetProperty("Study", out var studyElement))
         {
-            return JsonSerializer.Deserialize<StudyOperand>(json);
+            return JsonSerializer.Deserialize<StudyOperand>(json, options);
         }
 
         if (jsonElement.TryGetProperty("PriceAction", out var priceAction))
         {
-            return JsonSerializer.Deserialize<PriceActionOperand>(json);
+            return JsonSerializer.Deserialize<PriceActionOperand>(json, options);
         }
 
         if (jsonElement.TryGetProperty("Property", out var property))
         {
-            return JsonSerializer.Deserialize<PropertyOperand>(json);
+            return JsonSerializer.Deserialize<PropertyOperand>(json, options);
         }
 
         if (jsonElement.TryGetProperty("Value", out var value))
         {
-            return JsonSerializer.Deserialize<FixedOperand>(json);
+            return JsonSerializer.Deserialize<FixedOperand>(json, options);
         }
 
         return null;
