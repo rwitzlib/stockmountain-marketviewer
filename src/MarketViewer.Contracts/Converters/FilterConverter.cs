1cb2b5a15d1a6639d860c73f95be86951f8a5e5b
Rob Witzlib <61067692+rwitzlib@users.noreply.github.com>
2024-12-16 14:00:37 -0600

    remove unneeded libs

--- src/MarketViewer.Contracts/Converters/FilterConverter.cs
+++ src/MarketViewer.Contracts/Converters/FilterConverter.cs
@@ -1,5 +1,4 @@
-﻿using Amazon.DynamoDBv2;
-using MarketViewer.Contracts.Enums.Scan;
+﻿using MarketViewer.Contracts.Enums.Scan;
 using MarketViewer.Contracts.Models.Scan;
 using MarketViewer.Contracts.Models.ScanV2;
 using MarketViewer.Contracts.Models.ScanV2.Operands;
--- src/MarketViewer.Contracts/Models/Scan/ScanArgument.cs
+++ src/MarketViewer.Contracts/Models/Scan/ScanArgument.cs
@@ -1,6 +1,4 @@
 ﻿using MarketViewer.Contracts.Converters;
-using MarketViewer.Contracts.Models.Scan;
-using MarketViewer.Contracts.Models.ScanV2;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
