1e3f07da21d65487c997b02411ccba8fe1d56835
Rob Witzlib <61067692+rwitzlib@users.noreply.github.com>
2025-01-22 06:42:35 -0600

    add permissions middleware

---
+++ src/MarketViewer.Api/Authorization/RequiredPermissionsAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MarketViewer.Api.Authorization
+{
+    [ExcludeFromCodeCoverage]
+    [AttributeUsage(AttributeTargets.Method)]
+    public class RequiredPermissionsAttribute(string[] permission) : Attribute
+    {
+        public string[] Permission { get; } = permission;
+    }
+}
