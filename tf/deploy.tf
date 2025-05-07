7dea31f53426c0a56e5f7f37d8cc4a756def6444
Rob Witzlib <61067692+rwitzlib@users.noreply.github.com>
2025-05-06 20:52:34 -0500

    use null resource for http instead of data source

--- tf/deploy.tf
+++ tf/deploy.tf
@@ -1,22 +1,44 @@
-data "http" "deploy" {
-  url    = "https://management.stockmountain.io/api/deploy/start"
-  method = "POST"
+# data "http" "deploy" {
+#   url    = "https://management.stockmountain.io/api/deploy/start"
+#   method = "POST"
 
-  request_headers = {
-    Content-Type  = "application/json"
-    Authorization = "Bearer ${data.aws_ssm_parameter.deploy_token.value}"
+#   request_headers = {
+#     Content-Type  = "application/json"
+#     Authorization = "Bearer ${data.aws_ssm_parameter.deploy_token.value}"
+#   }
+
+#   request_body = jsonencode({
+#     id          = "${var.run_id}"
+#     environment = "${var.environment}"
+#     repository  = "stockmountain-marketviewer"
+#     file        = "deploy.docker-compose.yml"
+#     image       = data.aws_ecr_image.api.image_uri
+#     actor       = var.actor
+#   })
+# }
+
+# output "post_response_body" {
+#   value = data.http.deploy.body
+# }
+
+resource "null_resource" "deploy" {
+  triggers = {
+    run_id      = var.run_id
+    environment = var.environment
+    image_uri   = data.aws_ecr_image.api.image_uri
   }
 
-  request_body = jsonencode({
-    id          = "${var.run_id}"
-    environment = "${var.environment}"
-    repository  = "stockmountain-marketviewer"
-    file        = "deploy.docker-compose.yml"
-    image       = data.aws_ecr_image.api.image_uri
-    actor       = var.actor
-  })
+  provisioner "local-exec" {
+    command = <<-EOT
+      curl -X POST \
+      -H "Content-Type: application/json" \
+      -H "Authorization: Bearer ${data.aws_ssm_parameter.deploy_token.value}" \
+      -d '{"id":"${var.run_id}","environment":"${var.environment}","repository":"stockmountain-marketviewer","file":"deploy.docker-compose.yml","image":"${data.aws_ecr_image.api.image_uri}","actor":"${var.actor}"}' \
+      https://management.stockmountain.io/api/deploy/start
+    EOT
+  }
 }
 
-output "post_response_body" {
-  value = data.http.deploy.body
-}
+# output "deploy_id" {
+#   value = var.run_id
+# }
