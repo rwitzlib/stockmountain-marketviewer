205b457656ba47c2c06ee1d165a156279c27d4d8
Rob Witzlib <61067692+rwitzlib@users.noreply.github.com>
2025-05-06 22:27:17 -0500

    use output

--- tf/deploy.tf
+++ tf/deploy.tf
@@ -40,8 +40,9 @@ resource "null_resource" "deploy" {
           "file": "deploy.docker-compose.yml",
           "image": "${data.aws_ecr_image.api.image_uri}",
           "actor": "${var.actor}"
-        }'
-      EOT
+        }' > /tmp/deploy_response.txt
+      cat /tmp/deploy_response.txt | tail -n 1 > /tmp/deploy_status.txt
+    EOT
 
     environment = {
       TOKEN = data.aws_ssm_parameter.deploy_token.value
@@ -49,4 +50,13 @@ resource "null_resource" "deploy" {
 
     interpreter = ["/bin/bash", "-c"]
   }
+}
+
+data "local_file" "deploy_status" {
+  depends_on = [null_resource.deploy]
+  filename   = "/tmp/deploy_status.txt"
+}
+
+output "deploy_status_code" {
+  value = data.local_file.deploy_status.content
 }
