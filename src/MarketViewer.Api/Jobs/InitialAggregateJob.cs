aedcef057560c842e93c17b5961db01323fe4ede
Rob Witzlib <61067692+rwitzlib@users.noreply.github.com>
2025-05-19 09:48:01 -0500

    do proper latest minute math

--- src/MarketViewer.Api/Jobs/InitialAggregateJob.cs
+++ src/MarketViewer.Api/Jobs/InitialAggregateJob.cs
@@ -153,7 +153,7 @@ public class InitialAggregateJob(
             Entries = []
         };
 
-        var minuteTime = new DateTimeOffset(now.Year, now.Month, now.Day, now.Hour, now.Minute, 0, 0, now.Offset);
+        var minuteTime = new DateTimeOffset(now.Year, now.Month, now.Day, now.Hour, now.Minute - 1, 0, 0, now.Offset);
         var hourTime = new DateTimeOffset(minuteTime.Year, minuteTime.Month, minuteTime.Day, minuteTime.Hour, 0, 0, minuteTime.Offset);
 
         var tickers = marketCache.GetTickers();
